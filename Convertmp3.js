779b24097a6a8268070f3f5ab8e0531ae4c61816
gelmiyoga <insfollowers@gmail.com>
2017-03-19 11:02:35 +0700

    Create Convertmp3.js

---
+++ Convertmp3.js
@@ -0,0 +1,1 @@
+function YoutubeMp3Script(element,href){href=(href||element.getAttribute('href')).replace(/www.yt-mp3/,'api.yt-mp3');window.open(href,href,'height=327,width=100%,location=yes,menubar=no,resizable=no,scrollbars=no,status=no,toolbar=no');return false;}
